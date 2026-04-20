a8b7f382096457be83d3c163bf7fe4a5bf9c55fd
Arnav Ticku <arnav.eigenstudio@gmail.com>
2026-04-21 02:08:11 +0530

    deployment ready---firebaseadmin

--- src/utilities/firebaseAdmin.js
+++ src/utilities/firebaseAdmin.js
@@ -4,13 +4,10 @@ dotenv.config({
     path:"./.env"
 })
 
-import fs from "fs"
-import path from "path"
 
-const servicePath = path.resolve(process.env.FIREBASE_API_KEY)
 
 await admin.initializeApp({
-    credential: admin.credential.cert(JSON.parse( fs.readFileSync(servicePath , "utf-8") ))
+    credential: admin.credential.cert(JSON.parse(process.env.FIREBASE_API_KEY))
 });
 
 console.log("firebase admin initialized");
